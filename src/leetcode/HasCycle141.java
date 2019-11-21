1d2fb7ebc18247c620c6e853f0b6352934f0c99e
Fy <fuyu0824@pku.edu.cn>
2019-11-21 18:16:31 +0800

    leetcode 141

---
+++ src/leetcode/HasCycle141.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+//  判断环形链表。考研题目，使用快慢双指针。
+public class HasCycle141 {
+
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val = x;
+            next = null;
+        }
+    }
+    public boolean hasCycle(ListNode head){
+        if(head==null||head.next==null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(slow!=fast){
+            slow = slow.next;
+            if (fast.next != null && fast.next.next != null) {
+                fast = fast.next.next;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
