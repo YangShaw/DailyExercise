51299803a61b338761a8b8d759c51e13a3bd28ea
Fy <fuyu0824@pku.edu.cn>
2020-04-15 00:27:32 +0800

    next permutation 31

--- src/leetcode/NextPermutation31.java
+++ src/leetcode/NextPermutation31.java
@@ -12,48 +12,70 @@ public class NextPermutation31 {
         4 找不到这样的数，则全部升序排列
          */
         int n = nums.length;
-        int[] sorted = nums.clone();
-        Arrays.sort(sorted);
+        //int[] sorted = nums.clone();
+        //Arrays.sort(sorted);
+        //int indexOfMin = 0;
 
-        int indexOfMin = 0;
         boolean flag = false;
-        int i,j=0;
-        for(i=n-1;i>=0;--i){
-            if(indexOfMin>=n || nums[i]==sorted[indexOfMin]){
-                while(sorted[indexOfMin]==sorted[indexOfMin+1]){
-                    indexOfMin++;
-                }
-                indexOfMin++;
-                continue;
-            } else {
+        int i=0, j=0;
+        for(i = n-1;i>0;--i){
+            if(nums[i]>nums[i-1]){
                 flag = true;
-                for(j=i-1;j>=0;--j){
-                    if(nums[j]<nums[i]){
+                for(j=n-1;j>=0;--j){
+                    if(nums[j]>nums[i-1]){
+                        int temp = nums[i-1];
+                        nums[i-1] = nums[j];
+                        nums[j] = temp;
                         break;
                     }
                 }
                 break;
             }
         }
+
+        //for(i=n-1;i>=0;--i){
+        //    for(j=i-1;j>=0;--j){
+        //        if(nums[j]<nums[i]){
+        //            flag = true;
+        //            break;
+        //        }
+        //    }
+        //    if(flag){
+        //        break;
+        //    }
+
+            //if(indexOfMin>=n || nums[i]==sorted[indexOfMin]){
+            //    while(sorted[indexOfMin]==sorted[indexOfMin+1]){
+            //        indexOfMin++;
+            //    }
+            //    indexOfMin++;
+            //    continue;
+            //} else {
+            //    flag = true;
+            //    for(j=i-1;j>=0;--j){
+            //        if(nums[j]<nums[i]){
+            //            break;
+            //        }
+            //    }
+            //    break;
+            //}
+        //}
+
+        //  没有更小的排列了
         if(!flag){
             Arrays.sort(nums);
             return;
         }
 
         //  i:较大的数字； j:较小的数字
-        System.out.println(i);
-        System.out.println(j);
+        //System.out.println(i);
+        //System.out.println(j);
         List<Integer> temp = new ArrayList<>();
-        int pos = j;
-        for( ;j<n;++j){
-            if(j!=i){
-                System.out.println("add num" + nums[j]);
-                temp.add(nums[j]);
-            }
+        int pos = i;
+        for( ;i<n;++i){
+            temp.add(nums[i]);
         }
         Collections.sort(temp);
-        nums[pos] = nums[i];
-        pos++;
         int start = 0;
         for(; pos<n;++pos){
             nums[pos] = temp.get(start);
